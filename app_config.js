e93200d18048b1af64f6727d04b693dc124ba0be
Chandan Singh <chandan2495@gmail.com>
2016-03-01 15:03:23 +0530

    defaults changed

--- app_config.js
+++ app_config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	WEBCONSOLE_URL : 'http://devciweb.dm2.commvault.com/webconsole',	// example, http://setup1.commvault.com/webconsole
+	WEBCONSOLE_URL : 'http://setup1.abc.com/webconsole',	// example, http://setup1.abc.com/webconsole
 	PORT : 3000,	// listening port to create server
-	SYSTEM_IP_ADDRESS: '172.16.65.68',	//server machine ip address
-	VALIDITY_TIME : 1	// in minutes
+	SYSTEM_IP_ADDRESS: '127.0.0.0',	//(your current system ip address)
+	VALIDITY_TIME : 30	// in minutes
 };
